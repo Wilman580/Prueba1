51afb3210338644ba641c0447752edb9194632f1
Wilman580 <35231422+Wilman580@users.noreply.github.com>
2020-04-08 02:19:59 -0500

    Creación del proyecto
    Todos los proyectos incluidos

---
+++ Prueba1.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Prueba1.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Prueba1.Backend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Prueba1.Backend.MvcApplication" Language="C#" %>
